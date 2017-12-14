18d8a3ce866e9fb9d783cba783d1491d01c15428
jiashunming <jiashunming@outlook.com>
2017-12-14 14:07:56 +0800

    update: 添加wx测试token的验证

--- index.js
+++ index.js
@@ -1,55 +1,71 @@
-const Koa = require('koa');
-const Router = require('koa-router');
+const Koa = require('koa')
+const Router = require('koa-router')
+const crypto = require('crypto')
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
+const token = 'jarvisTokenTest'
 
 router.get('/', async function (ctx, next) {
-  // console.log(ctx);
-  // console.log('hello');
-  ctx.body = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-      <title>jarvis</title>
-      <style media="screen">
-        * {
-          margin: 0;
-          box-sizing: border-box;
-        }
-        body {
-          background-color: #108ee9;
-          color: #fff;
-          font-family: sans-serif;
-          display: flex;
-          align-items: center;
-          height: 100vh;
-          flex-flow: column;
-          justify-content: center;
-        }
+  // 添加wx的token验证
+  let {signature, timestamp, nonce, echostr} = ctx.query
+  if (signature) {
+    var oriArray = [nonce, timestamp, token]
+    oriArray.sort()
+    var original = oriArray.join('')
+    var scyptoString = sha1(original)
+    if (signature === scyptoString) {
+      ctx.body = echostr
+    } else {
+      ctx.body = 'false'
+    }
+  } else {
+    // console.log(ctx);
+    // console.log('hello');
+    ctx.body = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+        <title>jarvis</title>
+        <style media="screen">
+          * {
+            margin: 0;
+            box-sizing: border-box;
+          }
+          body {
+            background-color: #108ee9;
+            color: #fff;
+            font-family: sans-serif;
+            display: flex;
+            align-items: center;
+            height: 100vh;
+            flex-flow: column;
+            justify-content: center;
+          }
 
-        .title {
-          font-size: 24px;
-          font-weight: normal;
-          margin-bottom: 4em;
-        }
+          .title {
+            font-size: 24px;
+            font-weight: normal;
+            margin-bottom: 4em;
+          }
 
-        .sign {
-          font-size: 12px;
-          text-align: right;
-          width: 100%;
-          padding: 0 2em;
-        }
-      </style>
-    </head>
-    <body>
-      <h1 class="title">Hi there, website is in building.</h1>
-      <p class="sign">-- Jarvis<p>
-    </body>
-  </html>
-  `
+          .sign {
+            font-size: 12px;
+            text-align: right;
+            width: 100%;
+            padding: 0 2em;
+          }
+        </style>
+      </head>
+      <body>
+        <h1 class="title">Hi there, website is in building.</h1>
+        <p class="sign">-- Jarvis<p>
+      </body>
+    </html>
+    `
+  }
   // ctx.router available
 });
 
@@ -60,3 +76,10 @@ app
 app.listen(8000, () => {
   console.log('app run on http://127.0.0.1:8000');
 })
+
+function sha1 (str) {
+  var md5sum = crypto.createHash('sha1')
+  md5sum.update(str)
+  str = md5sum.digest('hex')
+  return str
+}
